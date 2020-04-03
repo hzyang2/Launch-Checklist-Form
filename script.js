ac15fdb3cec997908c2fade1a5a7365172ad8bcd
hzyang2 <61996071+hzyang2@users.noreply.github.com>
2020-04-03 18:16:08 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,20 @@
 </ol>
 <img src="${}">
 */
+function showDestination(json) {
+   const div = document.getElementById("missionTarget");   
+   div.innerHTML = `
+   <h2>Mission Destination</h2>
+   <div>
+      <ol>
+         <li>Name: ${json[0].name}</li>
+         <li>Diameter: ${json[0].diameter}</li>
+         <li>Star: ${json[0].star}</li>
+         <li>Distance from Earth: ${json[0].distance}</li>
+         <li>Number of Moons: ${json[0].moons}</li>
+      </ol>
+   </div>
+   
+   <img src=${json[4].image}></img>
+   `
+}
